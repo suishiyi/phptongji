f9ea1327e60150959a79d338fd3f09b4a383b3a3
suishiyi <77524001+suishiyi@users.noreply.github.com>
2022-06-15 04:06:11 +0800

    Update tongji.php

--- tongji.php
+++ tongji.php
@@ -58,6 +58,8 @@ var _hmt = _hmt || [];
   hm.src = "https://hm.baidu.com/hm.js?bff297dc194268a8f519bed00c0e7d69";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
+})();
+</script>
 
 临时统计<br>
 <a href="https://www.mfwztj.com/" target="_blank"><img src="https://www.mfwztj.com/hit.php?id=zmrkxkk&nd=6&style=1" border="0" alt="免费计数器"></a>
