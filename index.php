7959bd33b96e74b7193fc26df8278496a35a704c
suishiyi <77524001+suishiyi@users.noreply.github.com>
2022-06-15 03:51:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 tongji
+
+
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bff297dc194268a8f519bed00c0e7d69";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+
+
+<a href="https://www.mfwztj.com/" target="_blank"><img src="https://www.mfwztj.com/hit.php?id=zmrkxkk&nd=6&style=1" border="0" alt="免费计数器"></a>
