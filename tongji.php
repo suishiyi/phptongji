7f98091a1b93e02968f21b68b128a03ea7836862
suishiyi <77524001+suishiyi@users.noreply.github.com>
2022-06-15 04:04:03 +0800

    Update tongji.php

--- tongji.php
+++ tongji.php
@@ -35,3 +35,29 @@
 	
 </body>
 </html>
+
+
+tongji
+
+
+
+
+
+
+
+51啦统计https://sanliuyi101phptongji.herokuapp.com/  <br>
+<script type="text/javascript" src="//js.users.51.la/21350923.js"></script>
+
+
+
+百度统计https://sanliuyi101phptongji.herokuapp.com/  <br>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bff297dc194268a8f519bed00c0e7d69";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+
+临时统计<br>
+<a href="https://www.mfwztj.com/" target="_blank"><img src="https://www.mfwztj.com/hit.php?id=zmrkxkk&nd=6&style=1" border="0" alt="免费计数器"></a>
