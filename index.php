f517470c078fb2fc63ab6c102457877ccca75e9e
suishiyi <77524001+suishiyi@users.noreply.github.com>
2022-06-15 04:00:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 tongji
 
 
+
+
+
+
+
+51啦统计https://sanliuyi101phptongji.herokuapp.com/  <br>
+<script type="text/javascript" src="//js.users.51.la/21350923.js"></script>
+
+
+
+百度统计https://sanliuyi101phptongji.herokuapp.com/  <br>
 <script>
 var _hmt = _hmt || [];
 (function() {
@@ -9,5 +20,5 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 
-
+临时统计<br>
 <a href="https://www.mfwztj.com/" target="_blank"><img src="https://www.mfwztj.com/hit.php?id=zmrkxkk&nd=6&style=1" border="0" alt="免费计数器"></a>
