49ddeaa436dc4f95befd9179dfcbc18a88ef43d2
suishiyi <77524001+suishiyi@users.noreply.github.com>
2022-06-15 19:39:21 +0800

    update tongji.php

--- tongji.php
+++ tongji.php
@@ -45,10 +45,10 @@ header("content-type:text/html;charset=utf-8");
     echo "存在";
     while($count  === "")
    {
-    $count=file_get_contents("https://sanliuyi101phptongji.herokuapp.com/tongji.txt");  //取出文件tongji.txt中的内容
+    $count=file_get_contents("tongji.txt");  //取出文件tongji.txt中的内容
    }
   }
-      $count=file_get_contents("https://sanliuyi101phptongji.herokuapp.com/tongji.txt");  //取出文件tongji.txt中的内容
+      $count=file_get_contents("tongji.txt");  //取出文件tongji.txt中的内容
   $count= $count+1;
   echo "php".$count;
   
