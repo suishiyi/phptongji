4e64542598454072ee376d7e667e5798caf96b7a
suishiyi <77524001+suishiyi@users.noreply.github.com>
2022-06-15 22:52:24 +0800

    update tongji2.php

---
+++ tongji2.php
@@ -0,0 +1,19 @@
+
+
+php统计
+<?php
+header("content-type:text/html;charset=utf-8");
+if(file_exists("tongji2.txt")) {
+echo "存在";
+$count=file_get_contents("tongji.txt"); 
+$count= $count+1;
+echo "php".$count;
+$DOCUMENT_ROOT = $_SERVER['DOCUMENT_ROOT'];
+$fp = fopen("$DOCUMENT_ROOT/tongji2.txt",'w+');
+fclose($fp);
+//unlink("tongji.txt");
+file_put_contents("tongji.txt",$count); 
+?>
+
+
+
